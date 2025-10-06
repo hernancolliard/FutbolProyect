ad89fe7734a948a1b273773e9ca31e9c62ca82c3
hernan colliard <hernancolliard5@gmail.com>
2025-10-06 15:01:39 -0300

    Descripción de los cambios realizados

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -3,10 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
+  secure: process.env.EMAIL_PORT == 465, // true para 465, false para otros puertos como 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    // No fallar en certificados TLS inválidos (a veces necesario en entornos de servidor)
+    rejectUnauthorized: false,
   },
 });
 
