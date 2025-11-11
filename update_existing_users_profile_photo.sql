a6da1058fbae3dc377c64bb27a1167fec7257dd6
hernan colliard <hernancolliard5@gmail.com>
2025-11-11 19:32:49 -0300

    Descripción de los cambios realizados

--- update_existing_users_profile_photo.sql
+++ update_existing_users_profile_photo.sql
@@ -9,3 +9,4 @@ WHERE pu.id_usuario IS NULL;
 UPDATE perfiles_usuario
 SET foto_perfil_url = '/images/logos/logofpazul.webp'
 WHERE foto_perfil_url IS NULL OR foto_perfil_url = '';
+ALTER TABLE usuarios ADD COLUMN profile_views INTEGER DEFAULT 0;
