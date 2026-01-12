372b20d1fead5cad93005ee368c16e8069fb1532
hernan colliard <hernancolliard5@gmail.com>
2026-01-12 09:12:23 -0300

    fix(deploy): corregir rutas de importación y configuración de next.js

--- futbolproyect-nextjs/next.config.js
+++ futbolproyect-nextjs/next.config.js
@@ -2,9 +2,6 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbopack: false,
-  },
   webpack(config) {
     config.resolve.alias["@"] = path.resolve(__dirname, "./src");
     return config;
