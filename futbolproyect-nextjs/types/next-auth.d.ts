2dc4166dbf6939c8392f4b1b672617569d72897b
hernan colliard <hernancolliard5@gmail.com>
2026-01-14 09:28:20 -0300

    fix(nextjs): Add missing 'id' property to NextAuth types

--- futbolproyect-nextjs/types/next-auth.d.ts
+++ futbolproyect-nextjs/types/next-auth.d.ts
@@ -2,6 +2,7 @@ import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface User {
+    id: string;
     accessToken?: string;
     isadmin?: boolean;
     tipo_usuario?: string;
@@ -10,6 +11,7 @@ declare module "next-auth" {
   interface Session {
     accessToken?: string;
     user: {
+      id: string;
       accessToken?: string;
       isadmin?: boolean;
       tipo_usuario?: string;
@@ -19,6 +21,7 @@ declare module "next-auth" {
 
 declare module "next-auth/jwt" {
   interface JWT {
+    id: string;
     accessToken?: string;
     isadmin?: boolean;
     tipo_usuario?: string;
