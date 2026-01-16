01ad91b79a28adaa9ddaded15a82f99620fec9a8
hernan colliard <hernancolliard5@gmail.com>
2026-01-16 19:50:08 -0300

    fix(build): Corregir la importación de I18nProvider en layout.tsx

--- futbolproyect-nextjs/src/app/layout.tsx
+++ futbolproyect-nextjs/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import RootClientLayout from "@/components/layout/RootClientLayout"; // <--- Importamos el layout cliente
-import { I18nProvider } from "@/components/I18nProvider"; // Aseguramos las traducciones
+import I18nProvider from "@/components/I18nProvider"; // Aseguramos las traducciones
 
 const inter = Inter({ subsets: ["latin"] });
 
