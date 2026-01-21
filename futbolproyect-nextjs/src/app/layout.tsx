1a59d028aefb613328f3f2382c3c23f19e13a49a
hernan colliard <hernancolliard5@gmail.com>
2026-01-21 18:31:58 -0300

    fix(auth): Unificar sistema de autenticación a next-auth

--- futbolproyect-nextjs/src/app/layout.tsx
+++ futbolproyect-nextjs/src/app/layout.tsx
@@ -3,12 +3,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import RootClientLayout from "@/components/layout/RootClientLayout";
+import { SessionProvider } from "next-auth/react";
 import I18nProvider from "@/components/I18nProvider";
 // 1. Importamos el nuevo Registry
 import ThemeRegistry from "@/components/providers/ThemeRegistry";
 import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 import ParallaxClientProvider from "@/components/providers/ParallaxProvider";
-import { GoogleOAuthProvider } from '@react-oauth/google'; // Importar GoogleOAuthProvider
 import GoogleTagManager from "@/components/GoogleTagManager";
 import { Suspense } from "react";
 
@@ -36,8 +36,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ''; // Obtener Client ID
-
   return (
     <html lang="es">
       <body className={inter.className}>
@@ -48,12 +46,11 @@ export default function RootLayout({
           <ReactQueryProvider>
             <ThemeRegistry>
               <I18nProvider>
-                {/* Envolver AuthProvider con GoogleOAuthProvider */}
-                <GoogleOAuthProvider clientId={googleClientId}>
+                <SessionProvider>
                   <AuthProvider>
                     <RootClientLayout>{children}</RootClientLayout>
                   </AuthProvider>
-                </GoogleOAuthProvider>
+                </SessionProvider>
               </I18nProvider>
             </ThemeRegistry>
           </ReactQueryProvider>
