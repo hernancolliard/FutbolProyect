dada79fb5b0f828b1cb1c8dcbdd31d5a7eb7cc37
hernan colliard <hernancolliard5@gmail.com>
2026-02-01 04:10:26 -0300

    Descripción de los cambios realizados

--- futbolproyect-nextjs/src/app/layout.tsx
+++ futbolproyect-nextjs/src/app/layout.tsx
@@ -1,60 +1,43 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "@/context/AuthContext";
 import RootClientLayout from "@/components/layout/RootClientLayout";
 import I18nProvider from "@/components/I18nProvider";
-// 1. Importamos el nuevo Registry
 import ThemeRegistry from "@/components/providers/ThemeRegistry";
 import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 import ParallaxClientProvider from "@/components/providers/ParallaxProvider";
-import { GoogleOAuthProvider } from '@react-oauth/google'; // Importar GoogleOAuthProvider
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import GoogleTagManager from "@/components/GoogleTagManager";
 import { Suspense } from "react";
-
-export const dynamic = 'force-dynamic';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "FutbolProyect",
   description: "Conectando el mundo del fútbol",
-  icons: [
-    {
-      rel: 'icon',
-      url: '/images/logos/logofpblanco.png',
-      media: '(prefers-color-scheme: dark)',
-    },
-    {
-      rel: 'icon',
-      url: '/images/logos/logofpazul.png',
-      media: '(prefers-color-scheme: light)',
-    },
-  ],
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ''; // Obtener Client ID
+}) {
+  const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
 
   return (
     <html lang="es">
       <body className={inter.className}>
-        <Suspense fallback={<></>}>
+        <Suspense fallback={null}>
           <GoogleTagManager />
         </Suspense>
+
         <ParallaxClientProvider>
           <ReactQueryProvider>
             <ThemeRegistry>
               <I18nProvider>
-                {/* Envolver AuthProvider con GoogleOAuthProvider */}
                 <GoogleOAuthProvider clientId={googleClientId}>
-                  <AuthProvider>
-                    <RootClientLayout>{children}</RootClientLayout>
-                  </AuthProvider>
+                  {/* 👇 SIN AUTH GLOBAL */}
+                  <RootClientLayout>{children}</RootClientLayout>
                 </GoogleOAuthProvider>
               </I18nProvider>
             </ThemeRegistry>
